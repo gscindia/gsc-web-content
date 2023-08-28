5b743ac03c6befa6fda5df68d9281b8cb31e590d
Garalgacha Science Club <garalgachascienceclub.official@gmail.com>
2023-08-28 16:17:04 +0530

    student edit updated with option for gender update

--- gsc/gsc/src/main/java/org/gsc/medha/populator/DefaultFormADatatablePopulator.java
+++ gsc/gsc/src/main/java/org/gsc/medha/populator/DefaultFormADatatablePopulator.java
@@ -20,7 +20,7 @@ public class DefaultFormADatatablePopulator implements Populator<Candidate, Stri
 	public void populateAll(List<Candidate> source, List<String[]> target) {
 		source.forEach(s -> {
 			String[] str = new String[5];
-			str[0] = String.valueOf(s.getRoll());
+			str[0] = (s.getGender().equalsIgnoreCase("F")?"F":"").concat(s.getRoll()+"");
 			str[1] = "<a target='_blank' title='Print Admit Card' href='admit-card/" + s.getId() + "'>" + s.getName().toUpperCase()
 					+ "</a>&nbsp;" + "<i onClick='popModal("+s.getId()+");' class='material-icons tiny' title='Edit Details'>edit</i>"
 					+ "&nbsp;<i class='material-icons tiny ' title='Remove' data-id='" + s.getId()
