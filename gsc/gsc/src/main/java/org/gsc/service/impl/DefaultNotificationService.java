d7b1ffdf6efcc99d4346fb822653f20839df2486
Garalgacha Science Club <garalgachascienceclub.official@gmail.com>
2023-08-30 16:11:46 +0530

    updated props

--- gsc/gsc/src/main/java/org/gsc/service/impl/DefaultNotificationService.java
+++ gsc/gsc/src/main/java/org/gsc/service/impl/DefaultNotificationService.java
@@ -7,18 +7,22 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
 import org.gsc.service.NotificationService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 @Component("notificationService")
 public class DefaultNotificationService implements NotificationService {
-	
+	@Value("${gsc.medha.meta.whatsapp.accesstoken}")
+	String token;
+	@Value("${gsc.medha.meta.whatsapp.uri}")
+	String URI;
 	@Override
 	public String postWhatsAppNotification(String message) {
-		String token = "EAANjcvYOap8BO46V7did1wDrMl1t29ELvwSSLOSBz7ZBsOU6WbmIcEo9EJA8dSrip9GS6bDlL3pyuIodobarj10QQEsj2MwSZAKi0F0scyUUgtaizG5Qxc1Xd9JS1JzsvDAf3Sf21ZAL3aEjKksE7TyRvQC2LfSQ4zupQGJ6wjAqzZAFNWgaH0pJqy8Jh0TFGF9YiZBRMJXk2kt0hkKZAz";
 		try {
 			HttpRequest request = HttpRequest.newBuilder()
-					.uri(new URI("https://graph.facebook.com/v13.0/108679655668715/messages"))
-					.header("Authorization", "Bearer " + token).header("Content-Type", "application/json")
+					.uri(new URI(URI))
+					.header("Authorization", "Bearer " + token).header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
 					.POST(HttpRequest.BodyPublishers.ofString(
 							message))
 					.build();
